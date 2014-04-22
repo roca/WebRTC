1379120028bcf33d18b8013a75538be921e47649
roca <romelcampbell@gmail.com>
2014-04-22 12:20:09 -0400

    Code my first page. Seams to only work in firefox so far

---
+++ pluralsight/ps-webrtc-mediastream.js
@@ -0,0 +1,29 @@
+navigator.getWebcam = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+navigator.getWebcam(
+       //constraints
+       {video:true,audio:false},
+
+       //successCallback
+       gotWebcam,
+
+       //errorCallback
+       function(err){
+          console.log("Oops! Something's not right." + err)
+       }
+);
+
+
+function gotWebcam(stream){
+
+	
+	localVideo.src = window.URL.createObjectURL(stream);
+	localVideo.play();
+
+	var video_track = stream.getVideoTracks()[0];
+	var output = document.getElementById('output');
+	output.innerHTML = "stream id = " + stream.id + "<BR>";
+	output.innerHTML += "track readyState = " + video_track.readyState + "<BR>";
+	output.innerHTML += "stream id = " + video_track.id + "<BR>";
+	output.innerHTML += "stream id = " + video_track.kind + "<BR>";
+};
